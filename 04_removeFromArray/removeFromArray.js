dd625cba9d8a7307b1766fe92342688c7c4fbda8
modMars <malejandrosalcido@gmail.com>
2022-08-27 15:31:22 -0600

    Solve problems 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arr, ...rem) 
+    {
+        console.log(rem)
+        for(let i = 0; i < arr.length; i++)
+        {
+            for(let j = 0; j < rem.length; j++)
+            {
+                if(arr[i] === rem[j])
+                {
+                    arr.splice(i,1);
+                    i--; //The i-- is the key to solving this problem, you have to reduce the index because as the splice function removes values of the array the rest of the values shift to the left, if we were to leave the loop running we would skip the check on a couple of elements.
+                }
+            }
+        }
+        return arr;
+    }
+console.log(removeFromArray([1,2,3,4],3,2))
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let result = 0;
+    //Error catcher
+    if(int1 < 0 || int2 < 0 || typeof(int1) != 'number' || typeof(int2) != 'number')
+    {
+        return "ERROR";
+    }
+    //Compares both parameters
+    if(int1 < int2)
+    {
+        while(int1 <= int2)
+        {
+            result += int1;
+            int1++
+        }
+    }
+    else if(int1 > int2)
+    {
+        while(int1 >= int2)
+        {
+            result += int1;
+            int1--
+        }
+    }
+    return result;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
