f67c68276485fc4ac3ad3de3672fcda3ab91c8b0
modMars <malejandrosalcido@gmail.com>
2022-08-27 16:44:16 -0600

    Solve problem 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let result = (f-32)*5/9
+  result = Math.round(result*10)/10; //Rounding the result when f = 100, multiplying by 10 makes the result give us 378 instead of 38 since it's also multiplying the first decimal point which is .77777, and dividing it by 10 will make it round to 37.8.
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let result = c * 9/5 + 32
+  result = Math.round(result*10)/10; 
+  return result
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
