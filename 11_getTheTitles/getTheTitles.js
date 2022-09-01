f7749b24153f078bbe2346527f62cb95460fb686
modMars <malejandrosalcido@gmail.com>
2022-09-01 17:32:13 -0600

    Solve problem 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //The perfect function to achieve the goal is map() it basically goes through every object in the array, does whatever you tell the callback function to do and it saves the result into a new array, all i had to do was return each obj.title to make an array of all the titles.
+    titles = books.map((obj) => obj.title);
+    return titles;
 };
 
 // Do not edit below this line
