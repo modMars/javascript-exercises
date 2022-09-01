c7a11a67dcabb2e0e166089839491136d892a271
modMars <malejandrosalcido@gmail.com>
2022-09-01 15:40:25 -0600

    Add comment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,6 @@
 const palindromes = function (string) {
+    //First we need to process the parameter so it lower cases all the words  and also removes any special characters, to do this first we need to use string.toLowerCase() which returns a string with all its letters in lc. After that we used the string.replace() function which takes a regular expression (regex), and replaces every single match with whatever you tell it to, in this case i used a '' which is just an empty space.              a great tool to create the desired regex is this website https://regexr.com/. 
+    
     string = string.toLowerCase();
     string = string.replace(/\W/g , '')
     let newString = '';
