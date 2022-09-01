bc18ef17d9e0aa6c32bdcb7fcfc255c67d65adf6
modMars <malejandrosalcido@gmail.com>
2022-09-01 15:35:24 -0600

    Solve problem 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/\W/g , '')
+    let newString = '';
+    for(let i = string.length-1; i >= 0; i--)
+    {
+        newString += string[i]
+    }
+    if(newString == string)
+    {
+        return true;
+    }
+    else
+        return false
 };
+console.log(palindromes('Racecar!'))
 
 // Do not edit below this line
 module.exports = palindromes;
